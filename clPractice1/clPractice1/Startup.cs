c2ee309e7ac342f6fb104773d2c30f3030ac9840
titus <titus@DESKTOP-41NHQ2C>
2017-05-10 16:14:05 -0700

    1st commit

---
+++ clPractice1/clPractice1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(clPractice1.Startup))]
+namespace clPractice1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
